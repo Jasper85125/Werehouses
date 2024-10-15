a49dbf2b1106f7118090ec0ce38190520ec56509
Jasper van der Laan <75309576+Jasper85125@users.noreply.github.com>
2024-10-15 10:28:13 +0200

    Refactor C# controllers and services: Add ItemController and ItemService classes

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,12 +1,17 @@
+using item.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddControllers();
 
-builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<IItemService,ItemService>();
 
 var app = builder.Build();
+
+app.MapControllers();
 
 app.UseHttpsRedirection();
 
---
+++ C#/services/Iitemservice.cs
@@ -0,0 +1,10 @@
+public interface IItemService
+{
+    List<ItemCS> GetAllItems();
+    // ItemCS GetItem(int id);
+    // void CreateItem();
+    // void UpdateItem();
+    // void DeleteItem();
+    // void AttendItem();
+    // void ModifyItem();
+}
