7ad8393d5227d8197425d4d6e3cdedae60b27039
Jasper van der Laan <75309576+Jasper85125@users.noreply.github.com>
2024-10-18 15:35:03 +0200

    .

--- tests/itemsTests.cs
+++ tests/itemsTests.cs
@@ -1,8 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using item.Controllers;
-using item.Services;
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace item.Tests
@@ -41,6 +39,39 @@ namespace item.Tests
             Assert.IsInstanceOfType(okResult.Value, typeof(IEnumerable<ItemCS>));
             var returnedItems = okResult.Value as IEnumerable<ItemCS>;
             Assert.AreEqual(2, returnedItems.Count());
+        }
+
+        [TestMethod]
+        public void GetByUid_ReturnsOkResult_WithItem()
+        {
+            // Arrange
+            var item = new ItemCS { uid = "1", code = "Item1" };
+            _mockItemService.Setup(service => service.GetItemById("1")).Returns(item);
+
+            // Act
+            var result = _itemController.GetByUid("1");
+
+            // Assert
+            Assert.IsInstanceOfType(result.Result, typeof(OkObjectResult));
+            var okResult = result.Result as OkObjectResult;
+            Assert.IsNotNull(okResult);
+            Assert.IsInstanceOfType(okResult.Value, typeof(ItemCS));
+            var returnedItem = okResult.Value as ItemCS;
+            Assert.AreEqual("1", returnedItem.uid);
+            Assert.AreEqual("Item1", returnedItem.code);
+        }
+
+        [TestMethod]
+        public void GetByUid_ReturnsNotFound_WhenItemDoesNotExist()
+        {
+            // Arrange
+            _mockItemService.Setup(service => service.GetItemById("1")).Returns((ItemCS)null);
+
+            // Act
+            var result = _itemController.GetByUid("1");
+
+            // Assert
+            Assert.IsInstanceOfType(result.Result, typeof(NotFoundResult));
         }
     }
 }
