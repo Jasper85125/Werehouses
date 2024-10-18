ef794a2461e84c0e4533db8afe3ed42431a417aa
Stéphane Dauvillier <dauvillier04@gmail.com>
2024-10-18 13:24:13 +0200

    added a working test folder and made the GetAllWarehouses function

--- models/baseCS.cs
+++ models/baseCS.cs
@@ -6,6 +6,11 @@ public class BaseCS
     {
     }
 
+    public static string GetTimestamp()
+    {
+        return DateTime.UtcNow.ToString("u");
+    }
+
     public static DateTime GetTimestampCS()
     {
         return DateTime.UtcNow;
