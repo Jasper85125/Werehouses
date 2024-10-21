27c71733c09065762d5e753bec678be6b7ff9603
Jasper van der Laan <75309576+Jasper85125@users.noreply.github.com>
2024-10-21 14:18:26 +0200

    Update assembly version in tests

--- tests/obj/Debug/net8.0/tests.AssemblyInfo.cs
+++ tests/obj/Debug/net8.0/tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7daf3f295506ae684272d92c47471733df95fbde")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35aa076c0fc745f2266900f1cf544cbf960ec0c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
