daff5ea33010eae296512bbcfce959c45a7edb23
Hadyalt <1009748@hr.nl>
2024-10-22 14:48:07 +0200

    made create shipment and the test

--- Cargohub/services/shipmentservice.cs
+++ Cargohub/services/shipmentservice.cs
@@ -30,4 +30,20 @@ public class ShipmentService : IShipmentService
         ShipmentCS shipment = shipments.FirstOrDefault(ship => ship.Id == id);
         return shipment;
     }
+
+    public ShipmentCS CreateShipment(ShipmentCS newShipment)
+    {
+        var Path = "data/shipments.json";
+
+        List<ShipmentCS> shipments = GetAllShipments();
+
+        // Add the new shipment record to the list
+        newShipment.Id = shipments.Count > 0 ? shipments.Max(w => w.Id) + 1 : 1;
+        shipments.Add(newShipment);
+
+        // Serialize the updated list back to the JSON file
+        var jsonData = JsonConvert.SerializeObject(shipments, Formatting.Indented);
+        File.WriteAllText(Path, jsonData);
+        return newShipment;
+    }
 }
