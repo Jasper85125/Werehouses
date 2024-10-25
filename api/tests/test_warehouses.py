242318513838e133b9c93b7e190c062de9d59150
Jasper van der Laan <75309576+Jasper85125@users.noreply.github.com>
2024-10-25 12:21:34 +0200

    Refactor warehouse and location tests

--- api/tests/test_warehouses.py
+++ api/tests/test_warehouses.py
@@ -1,18 +1,5 @@
-from models.warehouses import Warehouses
 import unittest
 import httpx
-
-
-class TestWarehouses(unittest.TestCase):
-    def setUp(self) -> None:
-        self.warehouses = Warehouses("../data/")
-
-    def test_loaded(self):
-        self.assertGreater(len(self.warehouses.get_warehouses()), 0)
-
-
-if __name__ == "__main__":
-    unittest.main()
 
 
 def checkWarehouse(warehouse):
