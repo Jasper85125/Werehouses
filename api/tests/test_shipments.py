426ebf94ae1878bd8f1e7be1455ad547fbcd4c93
Githubjael <jaelimamdi@gmail.com>
2024-10-29 12:23:54 +0100

    it works...

--- api/tests/test_shipments.py
+++ api/tests/test_shipments.py
@@ -92,164 +92,145 @@
 import unittest
 import requests
 
+
 class TestClass(unittest.TestCase):
-    def SetUp():
+    def setUp(self):
         self.url = "http://localhost:3000/api/v1"
         self.headers = {'API_KEY': 'a1b2c3d4e5'}
-    
+
     def test_get_shipments(self):
-        response = requests.get(url=(self.url + "/shipments"), headers=self.headers)
+        response = requests.get(
+            url=(self.url + "/shipments"), headers=self.headers)
 
         self.assertEqual(response.status_code, 200)
-    
+
     def test_get_shipments_by_id(self):
-        response = requests.get(url=(self.url + "/shipments/1"), headers=self.headers)
+        response = requests.get(
+            url=(self.url + "/shipments/1"), headers=self.headers)
 
         self.assertEqual(response.status_code, 200)
-    
+
     def test_get_shipments_by_id_items(self):
-        response = requests.get(url=(self.url + "/shipments/1/orders"), headers=self.headers)
+        response = requests.get(
+            url=(self.url + "/shipments/1/items"), headers=self.headers)
 
         self.assertEqual(response.status_code, 200)
 
     def test_get_shipments_by_id_orders(self):
-        response = requests.get(url=(self.url + "/shipments/1/items"), headers=self.headers)
+        response = requests.get(
+            url=(self.url + "/shipments/1/orders"), headers=self.headers)
 
         self.assertEqual(response.status_code, 200)
 
     def test_post_shipment(self):
         data = {
-        "id": 9999,
-        "order_id": 9999,
-        "source_id": 9999,
-        "order_date": None,
-        "request_date": None,
-        "shipment_date": None,
-        "shipment_type": None,
-        "shipment_status": None,
-        "notes": None,
-        "carrier_code": None,
-        "carrier_description": None,
-        "service_code":None,
-        "payment_type": None,
-        "transfer_mode": None,
-        "total_package_count": 31,
-        "total_package_weight": 594.42,
-        "created_at": None,
-        "updated_at": None,
-        "items": [
+            "id": 9999,
+            "order_id": 9999,
+            "source_id": 9999,
+            "order_date": None,
+            "request_date": None,
+            "shipment_date": None,
+            "shipment_type": None,
+            "shipment_status": None,
+            "notes": None,
+            "carrier_code": None,
+            "carrier_description": None,
+            "service_code": None,
+            "payment_type": None,
+            "transfer_mode": None,
+            "total_package_count": 0,
+            "total_package_weight": 0,
+            "created_at": None,
+            "updated_at": None,
+            "items": [
                 {
-                    "item_id": "P007435",
-                    "amount": 23
+                    "item_id": "P000002",
+                    "amount": 2
                 },
                 {
-                    "item_id": "P009557",
+                    "item_id": "P000004",
                     "amount": 1
                 },
                 {
-                    "item_id": "P009553",
+                    "item_id": "P000006",
+                    "amount": 5
+                }
+            ]
+        }
+
+        response = requests.post(
+            url=(self.url + "/shipments"), headers=self.headers, json=data)
+
+        self.assertEqual(response.status_code, 201)
+
+    def test_put_shipment_by_id(self):
+        data = {
+            "id": 9999,
+            "order_id": 9999,
+            "source_id": 9999,
+            "order_date": None,
+            "request_date": None,
+            "shipment_date": None,
+            "shipment_type": None,
+            "shipment_status": None,
+            "notes": None,
+            "carrier_code": None,
+            "carrier_description": None,
+            "service_code": None,
+            "payment_type": None,
+            "transfer_mode": None,
+            "total_package_count": 0,
+            "total_package_weight": 0,
+            "created_at": None,
+            "updated_at": None,
+            "items": [
+                {
+                    "item_id": "P000002",
+                    "amount": 23
+                },
+                {
+                    "item_id": "P000004",
+                    "amount": 1
+                },
+                {
+                    "item_id": "P000006",
                     "amount": 50
                 }
             ]
         }
+        response = requests.put(
+            url=(self.url + "/shipments/9999"), headers=self.headers, json=data)
+        self.assertEqual(response.status_code, 200)
 
-        response = requests.post(url=(self.url + "/shipments"), headers=self.headers, json=data)
-
-        self.assertEqual(response.status_code, 201)
-
-    def test_put_shipment_by_id_(self):
+    def test_put_shipment_by_id_items(self):
         data = {
-        "id": 9999,
-        "order_id": 9999,
-        "source_id": 9999,
-        "order_date": None,
-        "request_date": None,
-        "shipment_date": None,
-        "shipment_type": None,
-        "shipment_status": None,
-        "notes": None,
-        "carrier_code": None,
-        "carrier_description": None,
-        "service_code":None,
-        "payment_type": None,
-        "transfer_mode": None,
-        "total_package_count": 1,
-        "total_package_weight": 5,
-        "created_at": None,
-        "updated_at": None,
-        "items": [
+            "items": [
                 {
-                    "item_id": "P007435",
-                    "amount": 2
+                    "item_id": "P000002",
+                    "amount": 230
                 },
                 {
-                    "item_id": "P009557",
-                    "amount": 1
+                    "item_id": "P000004",
+                    "amount": 100
                 },
                 {
-                    "item_id": "P009553",
-                    "amount": 5
+                    "item_id": "P000006",
+                    "amount": 500
                 }
             ]
         }
+        response = requests.put(
+            url=(self.url + "/shipments/9999/items"), headers=self.headers, json=data)
+        # print(response.status_code)
+        # print(response.text)
+        self.assertEqual(response.status_code, 200)
 
-        response = requests.put(url=(self.url + "/shipments/9999"), headers=self.headers, json=data)
-    def test_put_shipment_by_id_(self):
-        data = {
-        "id": "9999",
-        "order_id": 1,
-        "source_id": 9999,
-        "order_date": None,
-        "request_date": None,
-        "shipment_date": None,
-        "shipment_type": None,
-        "shipment_status": None,
-        "notes": None,
-        "carrier_code": None,
-        "carrier_description": None,
-        "service_code":None,
-        "payment_type": None,
-        "transfer_mode": None,
-        "total_package_count": 1,
-        "total_package_weight": 5,
-        "created_at": None,
-        "updated_at": None,
-        "items": [
-                {
-                    "item_id": "P007435",
-                    "amount": 2
-                },
-                {
-                    "item_id": "P009557",
-                    "amount": 1
-                },
-                {
-                    "item_id": "P009553",
-                    "amount": 5
-                }
-            ]
-        }
+    def test_delete_shipment_by_id(self):
+        response = requests.delete(
+            url=(self.url + "/shipments/9999"), headers=self.headers)
 
-        response = requests.put(url=(self.url + "/shipments/9999"), headers=self.headers, json=data)
-        def test_put_shipment_by_id_(self):
-        data = [
-                {
-                    "item_id": "P007435",
-                    "amount": 2
-                },
-                {
-                    "item_id": "P009557",
-                    "amount": 1
-                },
-                {
-                    "item_id": "P009553",
-                    "amount": 5
-                }
-            ]
+        self.assertEqual(response.status_code, 200)
 
-        response = requests.put(url=(self.url + "/shipments/9999"), headers=self.headers, json=data)
 
-        self.assertEqual(response.status_code, 201)
-
-# if __name__ == "__main__":
-#     unittest.main()
+if __name__ == "__main__":
+    unittest.main()
