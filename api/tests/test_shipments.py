13bb04024106880e271082e8cc5aa76568e042cb
Stéphane Dauvillier <dauvillier04@gmail.com>
2024-11-01 21:12:56 +0100

    made the put_shipment_by_id_commit

--- api/tests/test_shipments.py
+++ api/tests/test_shipments.py
@@ -229,6 +229,52 @@ class TestClass(unittest.TestCase):
         # print(response.status_code)
         # print(response.text)
         self.assertEqual(response.status_code, 200)
+    
+    #Werkt nog niet!
+    def test_put_shipment_by_id_orders(self):
+        data = {
+            
+        }
+        #response = requests.put(url=(self.url + "/shipments/1/orders"), headers=self.headers, json=data)
+        #self.assertEqual(response.status_code, 200)
+
+    def test_put_shipment_by_id_commit(self):
+        data = {
+                "id": 9999,
+            "order_id": 9999,
+            "source_id": 9999,
+            "order_date": None,
+            "request_date": None,
+            "shipment_date": None,
+            "shipment_type": None,
+            "shipment_status": None,
+            "notes": None,
+            "carrier_code": None,
+            "carrier_description": None,
+            "service_code": None,
+            "payment_type": None,
+            "transfer_mode": None,
+            "total_package_count": 0,
+            "total_package_weight": 0,
+            "created_at": None,
+            "updated_at": None,
+            "items": [
+                {
+                    "item_id": "P000002",
+                    "amount": 23
+                },
+                {
+                    "item_id": "P000004",
+                    "amount": 1
+                },
+                {
+                    "item_id": "P000006",
+                    "amount": 50
+                }
+            ]
+        }
+        response = requests.put(url = (self.url + "/shipments/1/commit"), headers = self.headers, json = data)
+        self.assertEqual(response.status_code, 200)
 
     def test_delete_shipment_by_id(self):
         response = requests.delete(
