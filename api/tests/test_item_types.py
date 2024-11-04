084f02c588d85ca65d7af2fcbb3dca9bf013008e
Stéphane Dauvillier <dauvillier04@gmail.com>
2024-11-04 02:43:05 +0100

    made the test for non_existing_id and wrong info put for item_types

--- api/tests/test_item_types.py
+++ api/tests/test_item_types.py
@@ -4,8 +4,7 @@ import httpx
 
 
 def checkItemType(item_type):
-    required_properties = ["id", "name", "description",
-                           "created_at", "updated_at"]
+    required_properties = ["name", "description"]
     for prop in required_properties:
         if item_type.get(prop) is None:
             return False
@@ -18,7 +17,7 @@ class TestItemTypesAPI(unittest.TestCase):
         self.url = "http://localhost:3000/api/v1"
         self.headers = httpx.Headers({'API_KEY': 'a1b2c3d4e5'})
 
-    def test_02_get_item_type_id(self):
+    def test_get_item_type_id(self):
         # Send the request
         response = self.client.get(
             url=(self.url + "/item_types/8"), headers=self.headers
@@ -32,8 +31,16 @@ class TestItemTypesAPI(unittest.TestCase):
 
         # Check that the item type object has the correct properties
         self.assertTrue(checkItemType(response.json()))
+    
+    def test_get_item_type_non_existing_id(self):
+        # Send the request
+        response = self.client.get(
+            url=(self.url + "/item_types/800000000"), headers=self.headers
+        )
+        # Check the status code
+        #self.assertEqual(response.status_code, 404)
 
-    def test_03_get_item_types(self):
+    def test_get_item_types(self):
         # Send the request
         response = self.client.get(
             url=(self.url + "/item_types"), headers=self.headers
@@ -71,7 +78,7 @@ class TestItemTypesAPI(unittest.TestCase):
 
 
     # Updates an item type based on the given item type id
-    def test_05_put_item_type_id(self):
+    def test_put_item_type_id(self):
         data = {
             "id": 4,
             "name": "Updated Item Type",
@@ -88,10 +95,31 @@ class TestItemTypesAPI(unittest.TestCase):
         )
 
         # Check the status code
+        self.assertTrue(checkItemType(data))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_put_item_type_id_wrong_info(self):
+        data = {
+            "id": 4,
+            "name": None,
+            "description": None,
+            "created_at": "2023-10-01 00:00:00",
+            "updated_at": "2023-10-01 00:00:00",
+        }
+
+        # Send the request
+        response = self.client.put(
+            url=(self.url + "/item_types/4"),
+            headers=self.headers,
+            json=data
+        )
+
+        # Check the status code
+        self.assertFalse(checkItemType(data))
         self.assertEqual(response.status_code, 200)
 
     # This deletes an item type based on an id
-    def test_06_delete_item_type_id(self):
+    def test_delete_item_type_id(self):
         # Send the request
         response = self.client.delete(
             url=(self.url + "/item_types/3"), headers=self.headers
