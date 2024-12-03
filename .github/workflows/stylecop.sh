12d3378aec11102a3e4812fca5693f59706cefe9
Hadyalt <100202784+Hadyalt@users.noreply.github.com>
2024-12-03 15:24:59 +0100

    Update stylecop.sh

--- .github/workflows/stylecop.sh
+++ .github/workflows/stylecop.sh
@@ -2,9 +2,15 @@
 cd ../
 cd ../
 mkdir -p tests/StyleCop
+
+# Loop through all C# files and run StyleCop checks
 for file in $(find . -type f -name "*.cs"); do
     filename=$(basename "$file" .cs)
     output_file="tests/StyleCop/${filename}_stylecop.txt"
-    dotnet build $file --no-restore --warnaserror > "$output_file"
+    
+    # Run dotnet build to trigger StyleCop checks and redirect the output to the file
+    dotnet build $file --no-restore --warnaserror > "$output_file" 2>&1
+    
+    # Log the file and the result file
     echo "$file -> $output_file" >> "$output_file"
 done
