8dff253ae0899ad3d457f39abc1f4bf36c30e34d
Jasper85125 <Jaspervanderlaan1105@gmail.com>
2025-01-14 14:16:41 +0100

    Refactor item tests: consolidate and update test cases for item retrieval and creation

--- PythonTests/test_Items.py
+++ PythonTests/test_Items.py
@@ -9,36 +9,9 @@ class TestClass(unittest.TestCase):
                          "http://localhost:5002/api/v2"]
         self.headers = httpx.Headers({'Api-Key': 'AdminKey'})
 
-    def test_02_get_item_id(self):
-        for url in self.versions:
-            with self.subTest(url=url):
-                response = self.client.get(
-                    url=(url + "/items/P000006"), headers=self.headers
-                )
-                self.assertEqual(response.status_code, 200)
-                self.assertEqual(type(response.json()), dict)
-
-    def test_03_get_items(self):
-        for url in ["http://localhost:5001/api/v1"]:
-            with self.subTest(url=url):
-                response = self.client.get(
-                    url=(url + "/items"), headers=self.headers
-                )
-                self.assertEqual(response.status_code, 200)
-                self.assertEqual(type(response.json()), list)
-
-    def test_03_get_items_v2(self):
-        for url in ["http://localhost:5002/api/v2"]:
-            with self.subTest(url=url):
-                response = self.client.get(
-                    url=(url + "/items"), headers=self.headers
-                )
-                self.assertEqual(response.status_code, 200)
-                self.assertEqual(type(response.json()), dict)
-
     def test_04_post_item(self):
         data = {
-            "uid": "P000001",
+            "uid": "",
             "item_line": 3,
             "item_group": 3,
             "item_type": 3,
@@ -61,36 +34,68 @@ class TestClass(unittest.TestCase):
                     msg=f"Failed to create item: {response.content}"
                 )
 
-    def test_05_put_item_id(self):
-        data = {
-            "uid": "P000003",
-            "item_line": 1,
-            "item_group": 1,
-            "item_type": 1,
-            "supplier_id": 1,
-            "name": "Updated Test Item",
-            "description": "This is an updated test item",
-            "code": "ITEM003",
-            "upc_code": "123456789013",
-            "model_number": "MODEL003",
-            "commodity_code": "COMM003",
-            "short_description": "Updated short description"
-        }
+    def test_02_get_item_id(self):
         for url in self.versions:
             with self.subTest(url=url):
-                response = self.client.put(
-                    url=(url + "/items/P000003"),
-                    headers=self.headers, json=data
+                response = self.client.get(
+                    url=(url + "/items"), headers=self.headers
                 )
                 self.assertEqual(response.status_code, 200)
+                items = response.json()
+                last_item_id = items[-1]['uid']
+                response = self.client.get(
+                    url=(url + f"/items/{last_item_id}"), headers=self.headers
+                )
+                self.assertEqual(response.status_code, 200)
+                self.assertEqual(type(response.json()), dict)
 
-    def test_06_delete_item_id(self):
-        for url in self.versions:
-            with self.subTest(url=url):
-                response = self.client.delete(
-                    url=(url + "/items/P000001"), headers=self.headers
-                )
-                self.assertEqual(response.status_code, 200)
+    # def test_03_get_items(self):
+    #     for url in ["http://localhost:5001/api/v1"]:
+    #         with self.subTest(url=url):
+    #             response = self.client.get(
+    #                 url=(url + "/items"), headers=self.headers
+    #             )
+    #             self.assertEqual(response.status_code, 200)
+    #             self.assertEqual(type(response.json()), list)
+
+    # def test_03_get_items_v2(self):
+    #     for url in ["http://localhost:5002/api/v2"]:
+    #         with self.subTest(url=url):
+    #             response = self.client.get(
+    #                 url=(url + "/items"), headers=self.headers
+    #             )
+    #             self.assertEqual(response.status_code, 200)
+    #             self.assertEqual(type(response.json()), dict)
+
+    # def test_05_put_item_id(self):
+    #     data = {
+    #         "item_line": 1,
+    #         "item_group": 1,
+    #         "item_type": 1,
+    #         "supplier_id": 1,
+    #         "name": "Updated Test Item",
+    #         "description": "This is an updated test item",
+    #         "code": "ITEM003",
+    #         "upc_code": "123456789013",
+    #         "model_number": "MODEL003",
+    #         "commodity_code": "COMM003",
+    #         "short_description": "Updated short description"
+    #     }
+    #     for url in self.versions:
+    #         with self.subTest(url=url):
+    #             response = self.client.put(
+    #                 url=(url + "/items/P000001"),
+    #                 headers=self.headers, json=data
+    #             )
+    #             self.assertEqual(response.status_code, 200)
+
+    # def test_06_delete_item_id(self):
+    #     for url in self.versions:
+    #         with self.subTest(url=url):
+    #             response = self.client.delete(
+    #                 url=(url + "/items/P000001"), headers=self.headers
+    #             )
+    #             self.assertEqual(response.status_code, 200)
 
 
 # to run the file: python -m unittest test_items.py
